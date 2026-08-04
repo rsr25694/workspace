e7f196de24073141edf6561165da1913c06a4088
Rahul Raskar <rahul.raskar5@gmail.com>
2026-08-05 03:46:44 +0530

    d

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'hi rauhl';
+$num=153;
+
+$sum=0;
+$temp=$num;
+
+while($temp>0){
+    $digit=$temp%10;
+    $sum += $digit*$digit*$digit;
+    $temp=floor($temp/10);
+}
+
+echo ($sum==$num) ? "Yes":"No";
